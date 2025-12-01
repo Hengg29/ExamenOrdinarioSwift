0fbea54cbd8b8e01d29dd8b10537f9abb4d0f105
Alexander Hernandez Henggeler <alexander.hernandez@iest.edu.mx>
2025-12-01 17:36:23 -0600

    Initial Commit

---
+++ OrdinarioExamen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OrdinarioExamen
+//
+//  Created by Alexander Hernandez Henggeler on 01/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OrdinarioExamen/OrdinarioExamenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrdinarioExamenApp.swift
+//  OrdinarioExamen
+//
+//  Created by Alexander Hernandez Henggeler on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OrdinarioExamenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
