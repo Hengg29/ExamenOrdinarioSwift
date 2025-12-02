f27ce6ce9c802c83c94c69f50688bab71a6bc096
Alexander Hernandez Henggeler <alexander.hernandez@iest.edu.mx>
2025-12-01 19:02:52 -0600

    first commit

---
+++ OrdinarioExamen/DemonSlayer.swift
@@ -0,0 +1,8 @@
+//
+//  DemonSlayer.swift
+//  OrdinarioExamen
+//
+//  Created by Alexander Hernandez Henggeler on 01/12/25.
+//
+
+import Foundation
---
+++ OrdinarioExamen/DetallesView.swift
@@ -0,0 +1,18 @@
+//
+//  DetallesView.swift
+//  OrdinarioExamen
+//
+//  Created by Alexander Hernandez Henggeler on 01/12/25.
+//
+
+import SwiftUI
+
+struct DetallesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetallesView()
+}
